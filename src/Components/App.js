3a7987bcefc15bef6b2e629bfa9728a7919bf2f0
mrgromov <mailrxtx@gmail.com>
2019-06-21 22:07:48 +0400

    hooks refactoring completed

---
+++ src/Components/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
+import useLocation from './useLocation'
+
+const App = () => {
+    const [lat, errorMessage] = useLocation()
+
+    const renderContent = () => {
+        if (errorMessage && !lat) {
+            return <div>Error: {errorMessage}</div>
+        }
+
+        if (!errorMessage && lat) {
+            return <SeasonDisplay lat={lat} />
+        }
+
+        return <Spinner message="Please accept location request" />
+    }
+
+    return <div className="border red">{renderContent()}</div>
+}
+
+export default App
